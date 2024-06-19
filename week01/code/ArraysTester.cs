c03d86ceeb483e834721fd11afb1f0ac469b8e37
ter21016 <blackblue58@hotmail.com>
2024-06-19 13:35:03 -0700

    add rotate list

--- week01/code/ArraysTester.cs
+++ week01/code/ArraysTester.cs
@@ -1,8 +1,10 @@
-public static class ArraysTester {
+public static class ArraysTester
+{
     /// <summary>
     /// Entry point for the tests
     /// </summary>
-    public static void Run() {
+    public static void Run()
+    {
         // Sample Test Cases (may not be comprehensive)
         Console.WriteLine("\n=========== PROBLEM 1 TESTS ===========");
         double[] multiples = MultiplesOf(7, 5);
@@ -39,9 +41,19 @@ public static class ArraysTester {
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return new double[0]; // replace this return statement with your own
+        //  Create an array of doubles 
+        double[] result = new double[length];
+
+        //  Use a loop to populate the array with multiples of the number
+        for (int i = 0; i < length; i++)
+        {
+            // Start with the initial number and multiply it by the loop index to generate each multiple
+            result[i] = number * (i + 1);
+        }
+        // return the array
+        return result; // replace this return statement with your own
     }
-    
+
     /// <summary>
     /// Rotate the 'data' to the right by the 'amount'.  For example, if the data is 
     /// <c>&lt;List&gt;{1, 2, 3, 4, 5, 6, 7, 8, 9}</c> and an amount is 3 then the list returned should be 
@@ -53,9 +65,32 @@ public static class ArraysTester {
     private static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
-        // Remember: Using comments in your program, write down your process for solving this problem
-        // step by step before you write the code. The plan should be clear enough that it could
-        // be implemented by another person.
+
+        List<int> rotatedArray = new List<int>();
+
+        int startIndex = data.Count - amount;
+
+        for (int i = startIndex; i < data.Count; i++)
+        {
+            int value = data[i];
+            rotatedArray.Add(value);
+        }
+
+        for (int j = startIndex - 1; j >= 0; j--)
+        {
+            int value = data[j];
+            rotatedArray.Add(value);
+        }
+
+        data.Clear();
+
+        data.AddRange(rotatedArray);
+
+
+
+
+
+
 
     }
 }
