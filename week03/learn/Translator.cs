108f6d21a11a356cfc3ee9fd8f73d269b631b44d
ter21016 <blackblue58@hotmail.com>
2024-07-01 18:32:58 -0700

    teacg03

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -25,6 +25,8 @@ public class Translator
     public void AddWord(string fromWord, string toWord)
     {
         // ADD YOUR CODE HERE
+         _words[fromWord] = toWord;
+        
     }
 
     /// <summary>
@@ -35,6 +37,13 @@ public class Translator
     public string Translate(string fromWord)
     {
         // ADD YOUR CODE HERE
-        return "";
+        if(_words.ContainsKey(fromWord))
+        {
+            return _words[fromWord];
+        } else {
+            _words[fromWord]  = "???" ;
+        
+        return _words[fromWord];
+        }
     }
 }
--- week03/teach/DisplaySums.cs
+++ week03/teach/DisplaySums.cs
@@ -1,5 +1,7 @@
-﻿public static class DisplaySums {
-    public static void Run() {
+﻿public static class DisplaySums
+{
+    public static void Run()
+    {
         DisplaySumPairs(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         // Should show something like (order does not matter):
         // 6 4
@@ -27,7 +29,20 @@
     /// in the list.
     /// </summary>
     /// <param name="numbers">array of integers</param>
-    private static void DisplaySumPairs(int[] numbers) {
+    private static void DisplaySumPairs(int[] numbers)
+    {
         // TODO Problem 2 - This should print pairs of numbers in the given array
+        HashSet<int> checkedNum = new HashSet<int>();
+
+        foreach (int num in numbers)
+        {
+            if (checkedNum.Contains(10 - num))
+            {
+                Console.WriteLine($"{num} {10 - num}");
+
+            }
+            checkedNum.Add(num);
+        }
+
     }
 }
